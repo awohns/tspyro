b9453a0d5d8b3690b0bdb68429df6b0d7b989fdb
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2022-03-23 12:01:22 -0400

    Drop 1-shifting in TimeDiffModel

--- tspyro/models.py
+++ tspyro/models.py
@@ -243,8 +243,6 @@ class TimeDiffModel(BaseModel):
             time = torch.zeros(self.num_nodes)
             time[..., self.is_internal] = self.prior_loc.exp()
             diff = self.cumsum_up_tree.inverse(time)
-            # Ensure parents are at least one generation older than children.
-            diff = (diff - 1).clamp(min=0.1)
             self.prior_diff_loc = diff[self.is_internal].log()
 
     def forward(self):
@@ -260,19 +258,18 @@ class TimeDiffModel(BaseModel):
             batch_shape = internal_diff.shape[:-1]
         diff = torch.zeros(batch_shape + self.is_internal.shape)
         diff[..., self.is_internal] = internal_diff
-        diff = diff + 1  # Parents are at least one generation older than children.
-        time = self.cumsum_up_tree(diff)  # sum version
-        # time = self.cumsum_up_tree(diff.exp()).log()  # softmax version
+        time = self.cumsum_up_tree(diff)
         pyro.deterministic("internal_time", time.detach()[..., self.is_internal])
 
         # Mutation part of the model.
-        gap = time[..., self.parent] - time[..., self.child]
+        gap = (time[..., self.parent] - time[..., self.child]).clamp(min=1)
         with edges_plate:
             rate = (gap * self.span * self.mutation_rate).clamp(min=1e-8)
             pyro.sample("mutations", dist.Poisson(rate), obs=self.mutations)
 
         # Geographic part of the model.
         location = None
+        migration_scale = None
         if self.migration_likelihood is not None:
             migration_scale = pyro.sample("migration_scale", dist.LogNormal(0, 4))
             with internal_nodes_plate:
